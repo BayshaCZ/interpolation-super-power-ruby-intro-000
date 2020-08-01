01296ad8a6471b554d493b70816c0bd91b281138
Eva Podbrdská <podbrdska@gmail.com>
2020-08-01 04:29:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,3 @@
 def display_rainbow(rainbow)
-  rainbow.map! do | color |
-    color = color.chr.upcase + ": " + color
-  end
-  puts rainbow.join(", ")
+  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{rainbow[6]}"
 end
